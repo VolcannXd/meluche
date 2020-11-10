51112ec38451c4ca1899415c33ce3e39b1769e56
Arthur Detaille <43517947+VolcannXd@users.noreply.github.com>
2020-11-10 09:06:46 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,71 @@
+firstnames =[
+    "Dane",
+    "Jeyffrey",
+    "David",
+    "Rory",
+    "Arthur",
+    "Tina",
+    "Tino",
+    "Hector",
+    "Charlotte",
+    "George",
+    "Victor",
+    "Romain",
+    "Aurelien",
+    "Bruce",
+    "Paul",
+    "Jean",
+    "Pierre",
+    "Claude",
+    "Steve",
+    "Francois",
+    "Louis",
+    "Booba",
+    "B2",
+    "Okou",
+    "Barack",
+    "Adolf",
+    "Benito",
+    "Joseph"
+]
+
+names = [
+    "Staline"
+    "Mussolini"
+    "Hitler",
+    "Gnakouri",
+    "Huile",
+    "Lacroix",
+    "Donier",
+    "Henri",
+    "Iov",
+    "Petit",
+    "Martin",
+    "Robin",
+    "Richard",
+    "Durand",
+    "Dubois",
+    "Moreau",
+    "Crillard",
+    "Noyer",
+    "Nisse",
+    "Guy-Pierre",
+    "Jules",
+    "Chateau",
+    "Fauve",
+    "Fourst",
+    "Proust",
+    "Hector-David",
+    "Brit"
+]
+
+domains = [
+    "google.com",
+    "hotmail.fr",
+    "orange.fr",
+    "laposte.net",
+    "wanadoo.fr",
+    "yahoo.com"
+]
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
